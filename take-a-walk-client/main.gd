30afbd88a6b89120912ea1bda69d610d820500e2
minho-jang <minho9301@naver.com>
2025-10-17 14:25:36 +0900

    add godot git plugin

--- take-a-walk-client/main.gd
+++ take-a-walk-client/main.gd
@@ -8,15 +8,12 @@ func _ready():
 
 
 func game_over():
-	
-	
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	
 	$Music.stop()
 	$DeathSound.play()
-
 
 func new_game():
 	score = 0
